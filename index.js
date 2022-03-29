15d03e1d20048861a370be9d37d2a23ba4267584
Tara Chand Kumawat <tarachand122333@gmail.com>
2022-03-29 22:43:28 +0530

    fix add task button and store memory and refresh

--- index.js
+++ index.js
@@ -1,7 +1,68 @@
-const task__container = document.querySelector(".task__container");
+const taskContainer = document.querySelector(".task__container");
 
-const saveChange = ()=>{
-    const taskDate = ()=>{
-        
-    }
-}
+let globalStore = [];
+
+const generateNewCard = (taskData) => `
+<div class="col-md-6 col-lg-4">
+<div class="card">
+  <div class="card-header d-flex justify-content-end gap-2">
+    <button type="button" class="btn btn-outline-success">
+      <i class="fas fa-pencil-alt"></i>
+    </button>
+    <button type="button" class="btn btn-outline-danger" id=${taskData.id} onclick="deleteCard.apply(this, arguments)">
+      <i class="fas fa-trash-alt" id=${taskData.id} onclick="deleteCard.apply(this, arguments)"></i>
+    </button>
+  </div>
+  <img
+    src=${taskData.imageUrl}
+    class="card-img-top"
+    alt="..."
+  />
+  <div class="card-body">
+    <h5 class="card-title">${taskData.taskTitle}</h5>
+    <p class="card-text">
+      ${taskData.taskDescription}
+    </p>
+    <a href="#" class="btn btn-primary">${taskData.taskType}</a>
+  </div>
+  <div class="card-footer">
+    <button type="button" class="btn btn-outline-primary float-end">
+      Open Task
+    </button>
+  </div>
+</div>
+</div>
+`;
+
+const loadInitialCardData = () => {
+    const getCardData = localStorage.getItem("tasky");
+    if(!getCardData) return;
+    
+    const {cards} = JSON.parse(getCardData);
+    
+
+    cards.map((cardObject) => {
+  
+      // inject it to DOM
+      taskContainer.insertAdjacentHTML("beforeend",  generateNewCard(cardObject));
+  
+      // update our globalStore
+      globalStore.push(cardObject);
+    })
+   
+  };
+
+const saveChanges = () => {
+  const taskData = {
+    id: `${Date.now()}`, // unique number for id
+    imageUrl: document.getElementById("imageurl").value,
+    taskTitle: document.getElementById("tasktitle").value,
+    taskType: document.getElementById("tasktype").value,
+    taskDescription: document.getElementById("taskdescription").value,
+  };
+
+  taskContainer.insertAdjacentHTML("beforeend", generateNewCard(taskData));
+  globalStore.push(taskData);
+  localStorage.setItem("tasky",JSON.stringify({cards:globalStore}));
+
+};
